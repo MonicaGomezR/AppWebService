5ec63860796ee0ea5fe5fd50553adbb6cb783197
Mónica Gómez <monicagomez251096@gmail.com>
2017-11-18 12:15:29 -0500

    App medio lista

---
+++ www/js/funciones.js
@@ -0,0 +1,19 @@
+$("#miformulario").submit(function(evento){
+	evento.preventDefault();
+
+	//console.info("Activando formulario....");
+	var cadena=$(this).serializeArray();
+	//console.info(cadena);
+
+	$.ajax({
+		url:"http://localhost:8080/prueba.php"
+
+
+	}).done(function(respuesta){
+		$(".mensaje").html(respuesta);
+
+
+	});
+
+});
+
